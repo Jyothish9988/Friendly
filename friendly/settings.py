828aa58e989251d8de0e9893bdf49d89f7f029be
Jyothish9988 <93032610+Jyothish9988@users.noreply.github.com>
2024-06-22 11:17:03 +0530

    update

--- friendly/settings.py
+++ friendly/settings.py
@@ -69,12 +69,12 @@ WSGI_APPLICATION = 'friendly.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -121,8 +121,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['.vercel.app'] # Allow *.vercel.app
 
-# ...
-
-DATABASES = {} # Prevent Django from loading an adapter
-
-# ...
